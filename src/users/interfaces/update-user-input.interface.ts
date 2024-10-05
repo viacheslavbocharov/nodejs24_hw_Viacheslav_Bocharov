13bb229933c4c997d12683fc2ba7b6eb4fb46d61
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-10-05 10:56:08 +0100

    add comments to interfaces

--- src/users/interfaces/update-user-input.interface.ts
+++ src/users/interfaces/update-user-input.interface.ts
@@ -1,3 +1,5 @@
 import { IUser } from './user.interface';
 
+//Omit<IUser, 'id'> создает новый тип, который содержит все поля интерфейса IUser, кроме поля id.
+//Partial преобразует все поля типа T в необязательные.
 export type IUpdateUserInput = Partial<Omit<IUser, 'id'>>;
--- src/users/interfaces/update-user-partial-input.interface.ts
+++ src/users/interfaces/update-user-partial-input.interface.ts
@@ -1,3 +1,3 @@
 import { IUpdateUserInput } from './update-user-input.interface';
-
+//Partial преобразует все поля типа T в необязательные.
 export type IUpdateUserPartialInput = Partial<IUpdateUserInput>;
