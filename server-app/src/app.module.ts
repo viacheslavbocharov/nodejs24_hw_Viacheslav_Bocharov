fd08b0f11f902eea1746974cb6c9aa659b424153
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-10-11 19:24:54 +0100

    add docker-compose

--- server-app/src/app.module.ts
+++ server-app/src/app.module.ts
@@ -1,17 +1,36 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  private readonly logger = new Logger(AppModule.name);
+
+  constructor() {
+    mongoose.connection.on('connected', () => {
+      this.logger.log('База данных успешно подключена!');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      this.logger.error(`Ошибка подключения к базе данных: ${err.message}`);
+    });
+  }
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware) // Применяем LoggerMiddleware
