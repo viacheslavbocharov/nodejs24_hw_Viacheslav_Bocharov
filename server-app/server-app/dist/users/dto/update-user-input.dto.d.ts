e6661488cfdac4dd8b19b924375e97d240793dfa
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-10-16 21:35:50 +0100

    del dist

--- server-app/server-app/dist/users/dto/update-user-input.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { IUpdateUserInput } from '../interfaces/update-user-input.interface';
-export declare class UpdateUserInputDto implements IUpdateUserInput {
-    firstName: string | null;
-    lastName: string | null;
-    age: number | null;
-    isStudent: boolean | null;
-}
